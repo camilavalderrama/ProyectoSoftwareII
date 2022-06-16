d3d573108cb68b0e55bd314169e42abd7e9c7684
camilavalderrama <63310050+camilavalderrama@users.noreply.github.com>
2022-06-16 02:59:49 -0500

    Create base.sql

---
+++ base.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Productos( id int(11) , nom_produc varchar(255), valor int(20), descripcion varchar(300) );
+
+
+INSERT INTO Productos VALUES (1, 'Agenda' , 25000 , 'agenda cinco materias'); 
+INSERT INTO Productos VALUES (2, 'Resaltadores' , 15500 , 'Caja resaltadores x6 unidades'); 
+INSERT INTO Productos VALUES (3, 'Marcadores' , 12000 , ' caja de marcadores x12'); 
+INSERT INTO Productos VALUES (4, 'Colores' , 8500 , 'caja de colores x 15 unidades'); 
+INSERT INTO Productos VALUES (5, 'Temperas' , 4000 , ' caja de temperas'); 
+INSERT INTO Productos VALUES (6, 'Esperos' , 6500 , 'esferos de colores'); 
+
+
+CREATE TABLE Cliente ( id_cli int(11), nombre varchar(50) , apellido varchar(50), correo varchar(50), celular int(20));
+
+
+INSERT INTO Cliente VALUES (1, 'camila', 'valderrama' , 'yariz.valderrama@uptc.edu.co', 987654); 
+INSERT INTO Cliente VALUES (1, 'Britne', 'vargas' , 'britne.vargas@uptc.edu.co', 123456); 
+
+
